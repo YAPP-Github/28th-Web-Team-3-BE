e9d8d22eff085cfbd83295a3f2d3493093650f04
d6nggyun <d6nggyun@gmail.com>
2026-07-24 00:05:27 +0900

    flyway : saving_record를 monthly_saving으로 교체하는 V6 추가 (#21)

--- api/src/test/kotlin/backend/yapp/api/mission/survey/MissionSurveyAcceptanceTest.kt
+++ api/src/test/kotlin/backend/yapp/api/mission/survey/MissionSurveyAcceptanceTest.kt
@@ -114,7 +114,7 @@ class MissionSurveyAcceptanceTest(
             )
 
         assertEquals(
-            listOf("1", "2", "3", "4", "5"),
+            listOf("1", "2", "3", "4", "5", "6"),
             queryStrings(
                 """
                     SELECT version
