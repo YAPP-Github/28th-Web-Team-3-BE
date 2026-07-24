0ba92e8a14f4302a141ca1bd8b64f760a01e1759
hoooonshub <yhn515@naver.com>
2026-07-24 15:52:08 +0900

    flyway : 미션 마이그레이션 버전 충돌 수정

--- api/src/test/kotlin/backend/yapp/api/mission/survey/MissionSurveyAcceptanceTest.kt
+++ api/src/test/kotlin/backend/yapp/api/mission/survey/MissionSurveyAcceptanceTest.kt
@@ -114,7 +114,7 @@ class MissionSurveyAcceptanceTest(
             )
 
         assertEquals(
-            listOf("1", "2", "3", "4", "5", "6", "7"),
+            listOf("1", "2", "3", "4", "5", "6", "7", "8", "9"),
             queryStrings(
                 """
                     SELECT version
